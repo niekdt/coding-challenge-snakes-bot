3050fcc6d17467017743514251c6a87985a95a04
Niek Den Teuling <niekdenteuling@gmail.com>
2023-09-21 10:43:03 +0200

    feat: change best eval

--- eval/best.py
+++ eval/best.py
@@ -17,9 +17,9 @@ def evaluate(board: Board, player: int) -> float:
         d1 = min([distance(board.player1_pos, candy_pos) for candy_pos in board.get_candies()])
         d2 = min([distance(board.player2_pos, candy_pos) for candy_pos in board.get_candies()])
 
-        score += (d2 - d1)
+        score += 10 * (d2 - d1)
 
-    # score += 10 * (distance(board.player2_pos, board.center) - distance(board.player1_pos, board.center))
+    score += 5 * (distance(board.player2_pos, board.center) - distance(board.player1_pos, board.center))
     score += distance(board.player1_pos, board.player2_pos)
 
     return player * score
